f3df59eb8c3be2c8185a603f7bc6aa7350aee7c1
Bhaveshsurti <bhaveshsurti007@gmail.com>
2025-07-16 18:24:31 +0530

    Update and rename style.css to style.js

--- style.js
+++ style.js
@@ -3,9 +3,9 @@ body {
   color: lime;
   text-align: center;
   font-family: monospace;
+  margin-top: 20px;
 }
 canvas {
   border: 2px solid lime;
   background-color: #111;
-  margin-top: 20px;
 }
